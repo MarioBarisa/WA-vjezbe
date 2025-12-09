944eb6a43812367496b890e7a81722bcfb520909
Mario Barisa <mario@barisa.me>
2025-12-09 18:38:08 +0100

    vjezbe 9.12 -  WA4

--- SamostalniZadatakZaVjezbu3/DostavljanjePizze/pizza-vue/src/components/OrderFooter.vue
+++ SamostalniZadatakZaVjezbu3/DostavljanjePizze/pizza-vue/src/components/OrderFooter.vue
@@ -2,22 +2,22 @@
 
 
 
-const props = defineProps({
+/*const props = defineProps({
     odabranaPizza: {
         type: Object,
         required: true
     }
-});
+});*/
 
 </script>
 
 
 <template>
-    <div class="flex items-center justify-center sm:justify-start flex-wrap gap-2 w-full sm:wauto">
+  <!--  <div class="flex items-center justify-center sm:justify-start flex-wrap gap-2 w-full sm:wauto">
         <button v-for="(cijena, velicina) in odabranaPizza.cijene" :key="velicina" class="px-3 py-1 cursor-pointer rounded-lg border border-slate-500 bg-slate-600/40
 hover:bg-orange-500 hover:border-orange-400 hover:text-white transition-all text-sm
 sm:text-base">
             {{ velicina }} – {{ cijena }}€
         </button>
-    </div>
+    </div>--> 
 </template>
