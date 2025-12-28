c8a46dcaff9bb6eab275fb682bfc98cf452267bf
Mario Barisa <mario@barisa.me>
2025-12-28 13:56:35 +0100

    dovršene vježbe iz WA-5

--- MongoDBvj1/index.js
+++ MongoDBvj1/index.js
@@ -25,7 +25,7 @@ let pizze_cursor = await db.collection('wa-test').find();
 let pizze = await pizze_cursor.toArray();
 
 
-app.get('/pizze', (req, res) => {
+app.get('/sve-pizze', (req, res) => {
     res.json(pizze).status(200);
 });
 
@@ -73,4 +73,45 @@ app.post('/narudzbe', async (req, res) => {
     catch (error) {
         res.status(400).send("Greška u dodavanju narudžbe. ");
     }
+});
+
+
+app.patch('/pizze/:naziv', async (req, res) => {
+    let pizze_collection = db.collection('wa-test');
+    let naziv_param = req.params.naziv;
+   
+    await pizze_collection.updateOne({ naziv: naziv_param }, { $set: { cijena: 0 } });
+    let pizza = await pizze_collection.findOne({ naziv: naziv_param });
+    res.status(200).json(pizza);
+}) 
+
+
+app.put('/pizze', async (req, res) => {
+    let pizze_collection = db.collection('wa-test');
+    let Meni = req.body;
+    try {
+        await pizze_collection.deleteMany({}); //brisanje svega prije novog inserta
+        let reasult = await pizze_collection.insertMany(Meni);
+        res.status(200).json({ insertedCount: reasult.insertedCount });
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}) 
+
+//QUERY I AGREGACIJA 
+
+app.get("/pizze", async (req, res) => {
+  let pizze_collection = db.collection('wa-test');
+  let cijena_query = req.query.cijena;
+  if (!cijena_query) {
+    let pizze = await pizze_collection.find().toArray(); // dohvaćamo sve pizze
+    return res.status(200).json(pizze);
+  }
+  try {
+    let pizze = await pizze_collection.find({ cijena: { $gte: Number(cijena_query) }}).toArray(); //VECE OD
+    res.status(200).json(pizze);
+  } catch (error) {
+    console.log(error.errorResponse);
+    res.status(400).json({ error: error.errorResponse });
+  }
 });
