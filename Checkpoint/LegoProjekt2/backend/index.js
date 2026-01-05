080fac080b69e4df710c0818b7588a77fe0738cb
Mario Barisa <mario@barisa.me>
2026-01-05 18:33:36 +0100

    dodan cors

--- Checkpoint/LegoProjekt2/backend/index.js
+++ Checkpoint/LegoProjekt2/backend/index.js
@@ -1,10 +1,11 @@
-
+import cors from 'cors';
 import express from "express";
 const app = express();
 import legoRoutes from "./routes/legoRoutes.js";
 
 app.use(express.json());                           
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(legoRoutes);                                   
 
 
