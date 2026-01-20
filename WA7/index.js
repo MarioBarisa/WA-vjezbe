7789ba8fc85085c275b21511be13e2373a17bd6c
Mario Barisa <mario@barisa.me>
2026-01-20 10:49:06 +0100

    inicjalni commit WA7

---
+++ WA7/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = 3000;
+
+
+app.get("/", (req, res) => {
+  res.send("Spremni za autentifikaciju!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Poslužitelj dela na portu ${PORT}`);
+});
