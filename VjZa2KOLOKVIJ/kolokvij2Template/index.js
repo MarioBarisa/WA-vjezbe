3390dd5a39fc1ef04f0a5d5fe2a7c92f91109786
Mario Barisa <mario@barisa.me>
2026-02-07 20:04:04 +0100

    napravljen kolokvij2 vanilla obrazac

---
+++ VjZa2KOLOKVIJ/kolokvij2Template/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+
+import { connectToDatabase } from "./db.js";
+
+const app = express();
+
+app.use(express.json());
+
+const db = await connectToDatabase(); // primjer korištenja asinkrone funkcije za spajanje na bazu podataka
+
+app.get("/", (req, res) => {
+  res.send("wa-final backend radi!");
+});
+
+const PORT = 3000; // pripazite da je port slobodan, ako nije, promijenite ga
+
+app.listen(PORT, (error) => {
+  if (error) {
+    console.log("Greška prilikom pokretanja poslužitelja", error);
+  }
+  console.log(`Poslužitelj sluša na http://localhost:${PORT}`);
+});
+
+
+
+
